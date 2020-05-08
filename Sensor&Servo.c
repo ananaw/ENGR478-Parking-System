6b6eb3ff085bf1d84d9c173bf41e4339240c340c
ananaw <61036678+ananaw@users.noreply.github.com>
2020-05-08 15:47:03 -0700

    Update Sensor&Servo.c

--- Sensor&Servo.c
+++ Sensor&Servo.c
@@ -53,31 +53,31 @@ ConfigurePWM(uint32_t period, uint32_t duty)
     //Configure PWM Clock divide system clock by 32
     SysCtlPWMClockSet(SYSCTL_PWMDIV_32);
 
-    // Enable the peripherals used by this program.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);  //Tiva Launchpad has two modules (0 and 1). Module 0 covers the B peripheral
+    // Enable E peripheral used by this program.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);  //Tiva Launchpad has two modules (0 and 1). Module 0 covers the E peripheral
 
-    //Configure PB6,PB7 Pins as PWM
-    GPIOPinConfigure(GPIO_PB6_M0PWM0);
-    GPIOPinConfigure(GPIO_PB7_M0PWM1);
-    GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6|GPIO_PIN_7);
+    //Configure PE4,PE5 Pins as PWM
+    GPIOPinConfigure(GPIO_PE4_M0PWM4);
+    GPIOPinConfigure(GPIO_PE5_M0PWM5);
+    GPIOPinTypePWM(GPIO_PORTE_BASE, GPIO_PIN_4|GPIO_PIN_5);
 
     //Configure PWM Options
-    //PWM_GEN_0 Covers M0PWM0 and M0PWM1 for PB6,7, with count down mode, no comparator update synchronization mode
-    PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN|PWM_GEN_MODE_NO_SYNC); 
+    //PWM_GEN_2 Covers M0PWM4 and M0PWM5 for PE4,5, with count down mode, no comparator update synchronization mode
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN|PWM_GEN_MODE_NO_SYNC); 
 
     //Set the Period (expressed in clock ticks)
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, period);
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, period);
 
-    //Set PWM duty for M0PWM0 and M0PWM1
-    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, duty);
-    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1, duty);
+    //Set PWM duty for M0PWM4 and M0PWM5
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, duty);
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_5, duty);
 
     // Enable the PWM generator
-    PWMGenEnable(PWM0_BASE, PWM_GEN_0);
+    PWMGenEnable(PWM0_BASE, PWM_GEN_2);
 
-		// Enable PWM Output pins 6 & 7: M0PWM0 and M0PWM1
-    PWMOutputState(PWM0_BASE, PWM_OUT_0_BIT|PWM_OUT_1_BIT, true);		
+		// Enable PWM Output pins 4 & 5: M0PWM4 and M0PWM5
+    PWMOutputState(PWM0_BASE, PWM_OUT_4_BIT|PWM_OUT_5_BIT, true);		
 }
 
 void Timer0A_Init(unsigned long reload)
@@ -96,8 +96,9 @@ void Interrupt_Init(void)
 {
 	IntEnable(INT_GPIOF);								                          // Enable interrupt 30 in NVIC, Code: NVIC_EN0_R |= 0x40000000;
 	IntPrioritySet(INT_GPIOF, 0x00);		                          // Set interrupt priority 0 bits 23-21:010, Code: NVIC_PRI7_R &= ~0x00E00000
-	GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_2);		//GPIO_PORTF_IM_R |= 0x04; arm interrupt on PF2 (Echo)
+	GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_1|GPIO_INT_PIN_2);		//GPIO_PORTF_IM_R |= 0x04; arm interrupt on PF2 (Echo)
 	GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_RISING_EDGE);
+	GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_BOTH_EDGES);
 	                                                              //GPIO_PORTF_IS_R &= ~0x04;	PF2 are edge sensitive
 	                                                              //GPIO_PORTF_IBE_R &= ~0x04;  PF2 not both edge triggered, depends on IEV register
 	                                                              //GPIO_PORTF_IEV_R |= 0x04;PF2 are rising-edged event
@@ -129,12 +130,9 @@ void GPIOPortF_Handler(void)
 		if(count < 100)
 		{
 			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
-			
-      cars_count++;
       
-			//Drive servo to 90 left (2.6ms, or 650)	
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 625);
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1, 625);
+			//Drive servo to 90 left (2.5ms, or 625)	
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, 625);
 			
 			// 2s delay, 5333334
 			SysCtlDelay((2*SysCtlClockGet()-2)/3 + 1);
@@ -144,10 +142,22 @@ void GPIOPortF_Handler(void)
 		{
 			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
 
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 375);
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1, 375);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, 375);
 		}
 		count = 0;	
+		
+		if(GPIO_PORTF_RIS_R&0x02)
+		{
+			GPIOIntClear(GPIO_PORTF_BASE, GPIO_INT_PIN_1);
+			if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1)==GPIO_PIN_1)
+			{
+				cars_count++;
+			}
+			else
+			{
+				cars_count--;
+			}
+		}
 }
 
 int main(void)
